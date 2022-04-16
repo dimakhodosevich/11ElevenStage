2f39905398f0ff5317df7769df26c901bd0354d3
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-16 14:48:18 +0300

    Make autoformatting.

--- src/by/itstep/khodosevich/elevenstageproject/task02/controller/ControllerStructureOfValue.java
+++ src/by/itstep/khodosevich/elevenstageproject/task02/controller/ControllerStructureOfValue.java
@@ -1,13 +1,15 @@
 package by.itstep.khodosevich.elevenstageproject.task02.controller;
 
 import by.itstep.khodosevich.elevenstageproject.view.Printer;
+
 import static by.itstep.khodosevich.elevenstageproject.task02.module.logic.StructureOfValue.*;
+
 import java.util.Scanner;
 
 
 public class ControllerStructureOfValue {
 
-    private static final Scanner SCANNER =new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         Printer.print("Input your value: ");
