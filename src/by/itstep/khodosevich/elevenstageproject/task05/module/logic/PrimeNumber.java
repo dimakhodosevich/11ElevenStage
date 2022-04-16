dac65240a89870f1e95c301d32468ffb949f65f0
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-16 21:01:09 +0300

    Start task05, make test for it. Create PrimeNumber plug logic.

--- src/by/itstep/khodosevich/elevenstageproject/task05/module/logic/PrimeNumber.java
+++ src/by/itstep/khodosevich/elevenstageproject/task05/module/logic/PrimeNumber.java
@@ -1,4 +1,9 @@
 package by.itstep.khodosevich.elevenstageproject.task05.module.logic;
 
 public class PrimeNumber {
+    private PrimeNumber(){}
+
+    public static boolean isPrimeNumber(double number){
+        return false;
+    }
 }
--- test/by.itstep.khodosevich.elevenstageproject/task05/PrimeNumberTest.java
+++ test/by.itstep.khodosevich.elevenstageproject/task05/PrimeNumberTest.java
@@ -1,4 +1,43 @@
 package by.itstep.khodosevich.elevenstageproject.task05;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.elevenstageproject.task05.module.logic.PrimeNumber.*;
+import static by.itstep.khodosevich.elevenstageproject.view.Printer.*;
+
 public class PrimeNumberTest {
+
+    @Test
+    public void testIsPrimeNumberPositive(){
+        int actual_number = 7;
+
+        boolean actual = isPrimeNumber(actual_number);
+        assertTrue(ERROR_MESSAGE, actual);
+    }
+
+    @Test
+    public void testIsPrimeNumberNegative(){
+        int actual_number = 9;
+
+        boolean actual = isPrimeNumber(actual_number);
+        assertFalse(ERROR_MESSAGE, actual);
+    }
+
+    @Test (expected =RuntimeException.class)
+    public void testIsPrimeNumberWithZero(){
+        int actual_number = 0;
+
+        isPrimeNumber(actual_number);
+    }
+
+    @Test (expected =RuntimeException.class)
+    public void testIsPrimeNumberWithONe(){
+        int actual_number = 1;
+
+        isPrimeNumber(actual_number);
+    }
+
+
+
+
 }
