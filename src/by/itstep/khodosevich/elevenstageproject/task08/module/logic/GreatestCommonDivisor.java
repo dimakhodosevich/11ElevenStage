893dfb015e573f1e4e8f6b596ac6939f99ce8f9e
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-17 20:23:15 +0300

    Start task08. Make unit test. Create plug method for find GreatestCommonDivisor.

---
+++ src/by/itstep/khodosevich/elevenstageproject/task08/module/logic/GreatestCommonDivisor.java
@@ -0,0 +1,19 @@
+package by.itstep.khodosevich.elevenstageproject.task08.module.logic;
+
+public class GreatestCommonDivisor {
+
+    private static final String ERROR;
+
+    static{
+        ERROR ="You entered unaccepted number!!!";
+    }
+
+    private GreatestCommonDivisor(){
+
+    }
+
+    public static int  getGreatestCommonDivisor(int var1, int var2){
+        return -1;
+    }
+
+}
